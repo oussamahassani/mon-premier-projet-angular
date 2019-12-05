fded8f6a3ad871b9418afb1f6ac4c598e3368038
oussamahassani <hassani20120@gmail.com>
2019-12-05 13:49:02 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mon-premier-projet';
+  isAuth = false;
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+    appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+constructor() {
+ setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+}
+onAllumer() {
+    console.log('On allume tout !');
+}
+
+
+}
